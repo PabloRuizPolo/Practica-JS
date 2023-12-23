436fb63dfe0f4026771dbe235c47ebfc07e657f4
Pablo Ruiz <pablo.ruiz.polo@gmail.com>
2023-12-23 18:19:04 +0100

    Ejercicio 2: Bug

---
+++ bug.js
@@ -0,0 +1,28 @@
+/* Corrige el bug del siguiente código:
+const calcularPromedio = (numeros) => {
+    let sumaTotal = 0;
+
+    for (let i = 0; i <= numeros.length; i++) {
+        sumaTotal += numeros[i];
+    }
+
+    const promedio = sumaTotal / numeros.length;
+    return promedio;
+}
+
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros); */
+
+const calcularPromedio = (numeros) => {
+    let sumaTotal = 0;
+
+    for (let i = 0; i < numeros.length; i++) { //queremos que sea menor que el length de la lista, no menor o igual
+        sumaTotal += numeros[i];
+    }
+
+    const promedio = sumaTotal / numeros.length;
+    return promedio;
+}
+
+const listaNumeros = [1, 2, 3, 4, 5];
+const promedioNumeros = calcularPromedio(listaNumeros);
